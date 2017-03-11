1309c71267257968f3986ee73f45f9d6d6c7c888
Micah <Micah>
2017-03-11 10:44:11 -0800

    Testing Ground:

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,87 @@
-var createSearchResults = function(stuff){
-	console.log(queryURL);
+var player;
+var video = "";
+var playlist = ""
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: video,
+      playerVars: {		modestbranding: 1,'playlist': playlist, autoplay: 0, showinfo: 1	},
+      events: {
+        'onReady': function (event) {
+	        event.target.playVideo();
+	      },
+        'onStateChange': function (event) {
+	        if (event.data == YT.PlayerState.PLAYING && !done) {
+	          setTimeout(function () {
+		        player.stopVideo();
+		      }, 6000);
+	          done = true;
+	        }
+	      }
+      }
+
+    });
 }
 
-var search = "Apple";
-var apiKey = "AIzaSyC6KOmJ_6LXQJg_fa5qwpl1L20JWwW-NiY";
+var addPlayer = function(){
+			// 2. This code loads the IFrame Player API code asynchronously.
+	      // console.log(playlist);
+	      var tag = document.createElement('script');
 
-var queryURL = "https://www.googleapis.com/youtube/v3/search?" + 
-       //"part=snippet" +
-        //Orders by view count
-        //"&order=viewCount" +
+	      tag.src = "https://www.youtube.com/iframe_api";
+	      var firstScriptTag = document.getElementsByTagName('script')[0];
+	      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	      // 3. This function creates an <iframe> (and YouTube player)
+	      //    after the API code downloads.
+	      var done = false;
+
+	      //todo use the playlist arg instead
+			
+		  onYouTubeIframeAPIReady(playlist,video);
+	      
+	      // 4. The API will call this function when the video player is ready.
+	      
+
+	      // 5. The API calls this function when the player's state changes.
+	      //    The function indicates that when playing a video (state=1),
+	      //    the player should play for six seconds and then stop.
+	      
+	      $('body').append(tag);
+}
+
+// var p = 'NS0txu_Kzl8,5dsGWM5XGdg,tntOCGkgt98,M7lc1UVf-VE';
+// addPlayer(p);
+
+$(document).ready(function(){
+	var search = "Acoustic Kitty";
+	var apiKey = "AIzaSyC6KOmJ_6LXQJg_fa5qwpl1L20JWwW-NiY";
+
+	var queryURL = "https://www.googleapis.com/youtube/v3/search?" + 
+		//Search query
         "&q=" + encodeURI(search) +
-        "&part=player" +
-        //Type is video
-        //"&type=video" + 
-        //Video has hugh defintion
-        //"&videoDefinition=high";
+        //part type
+        "&part=snippet" +
+        //Api key
+        "&type=video" +
+        "&videoEmbeddable=true" +
+        "&videoSyndicated=true" +
         "&key=" + apiKey;
-        //https://www.googleapis.com/youtube/v3/search?q=cat&part=snippet&key=AIzaSyC6KOmJ_6LXQJg_fa5qwpl1L20JWwW-NiY
 
-$.ajax({
+        //addPlayer('NS0txu_Kzl8,5dsGWM5XGdg,tntOCGkgt98,M7lc1UVf-VE');
+	$.ajax({
 		url: queryURL, 
 		method: 'GET'
-}).done(createSearchResults);
+	}).done(function(response){
+		playlist = response.items[0].id.videoId;
+		video = response.items[0].id.videoId;
+		for (var i = 1; i < response.items.length; i++) {
+			playlist += "," + response.items[i].id.videoId;
+		}
+		console.log(playlist);
+		addPlayer(playlist, video);
+	});
+})
+
